a1cfa2e7125e7d4046318ecaa794f935edb39dcd
ArdeshirV <e-job@protonmail.com>
2025-09-03 14:05:09 +0200

    Update

--- main.go
+++ main.go
@@ -9,118 +9,118 @@ import (
 
 func main() {
 	fmt.Println(Prompt("My HTTP Server"))
-  myServer := new(myserver)
-  myServer.GET("/hello/a/b/c/:num", myHelloHandler)
-  http.ListenAndServe("localhost:8080", myServer)
+	myServer := new(myserver)
+	myServer.GET("/hello", myHelloHandler)
+	http.ListenAndServe("localhost:8080", myServer)
 }
 
 type MyHandler func(server requestResponse)
 
 type MyServer interface {
-  GET(route string, handler MyHandler)
-  POST(route string, handler MyHandler)
-  PATCH(route string, handler MyHandler)
-  PUT(route string, handler MyHandler)
-  DELETE(route string, handler MyHandler)
+	GET(route string, handler MyHandler)
+	POST(route string, handler MyHandler)
+	PATCH(route string, handler MyHandler)
+	PUT(route string, handler MyHandler)
+	DELETE(route string, handler MyHandler)
 }
 
 type RequestResponse interface {
-  Request() *http.Request 
-  Response() http.ResponseWriter
+	Request() *http.Request
+	Response() http.ResponseWriter
 }
 
 type myserver struct {
-  routes []Route
+	routes []Route
 }
 
 type Route struct {
-  parts []string
-  handler MyHandler
-  method string 
+	parts   []string
+	handler MyHandler
+	method  string
 }
 
 func NewServer() *myserver {
-  return &myserver{
-    routes: []Route{},
-  }
+	return &myserver{
+		routes: []Route{},
+	}
 }
 
 func (s *myserver) route(method, route string, handler MyHandler) {
-  s.routes = append(s.routes, Route{
-    parts: strings.Split(route, "/"),
-    handler: handler,
-    method: method,
-  })
+	s.routes = append(s.routes, Route{
+		parts:   strings.Split(route, "/"),
+		handler: handler,
+		method:  method,
+	})
 }
 
 func (s *myserver) GET(route string, handler MyHandler) {
-  s.route(http.MethodGet, route, handler)
+	s.route(http.MethodGet, route, handler)
 }
 
 func (s *myserver) POST(route string, handler MyHandler) {
-  s.route(http.MethodPost, route, handler)
+	s.route(http.MethodPost, route, handler)
 }
 
 func (s *myserver) PATCH(route string, handler MyHandler) {
-  s.route(http.MethodPatch, route, handler)
+	s.route(http.MethodPatch, route, handler)
 }
 
 func (s *myserver) PUT(route string, handler MyHandler) {
-  s.route(http.MethodPut, route, handler)
+	s.route(http.MethodPut, route, handler)
 }
 
 func (s *myserver) DELETE(route string, handler MyHandler) {
-  s.route(http.MethodDelete, route, handler)
+	s.route(http.MethodDelete, route, handler)
 }
 
 type requestResponse struct {
-  request *http.Request
-  response http.ResponseWriter
+	request  *http.Request
+	response http.ResponseWriter
 }
 
 func (s *requestResponse) Request() *http.Request {
-  return s.request
+	return s.request
 }
 
 func (s *requestResponse) Response() http.ResponseWriter {
-  return s.response
+	return s.response
 }
 
 func (s *myserver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  server := &requestResponse{request: r, response: w}
-  urlParts := strings.Split(r.URL.Path, "/")
-  for _, route := range s.routes {
-    if len(route.parts) != len(urlParts) {
-      continue
-    }
-    for _, urlPart := range urlParts {
-      for _, urlParam := range route.parts {
-        if urlPart != urlParam && !strings.HasPrefix(urlParam, ":") {
-          continue
-        }
-      }
-    }
+	server := &requestResponse{request: r, response: w}
+	urlParts := strings.Split(r.URL.Path, "/")
+	for _, route := range s.routes {
+		if len(route.parts) != len(urlParts) {
+			continue
+		}
+		for _, urlPart := range urlParts {
+			for _, urlParam := range route.parts {
+				if urlPart != urlParam && !strings.HasPrefix(urlParam, ":") {
+					continue
+				}
+			}
+		}
 
-    if r.Method != route.method {
-      w.WriteHeader(http.StatusMethodNotAllowed)
-      fmt.Fprintf(w, "Method not allowd")
-      return
-    }
+		if r.Method != route.method {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method not allowd")
+			return
+		}
 
-    route.handler(server)
-    return
-  }
+		route.handler(*server)
+		return
+	}
 }
 
 func myHelloHandler(s requestResponse) {
-  fmt.Fprint(s.Response(), "This is a new world!")
+	fmt.Fprint(s.Response(), "This is a new world!")
 }
 
 func logger(next http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    t := time.Now()
-    time.Sleep(time.Second)
-    next.ServeHTTP(w, r)
-    fmt.Println("Took:", time.Since(t))
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		time.Sleep(time.Second)
+		next.ServeHTTP(w, r)
+		fmt.Println("Took:", time.Since(t))
+	})
 }
