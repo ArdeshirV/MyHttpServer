fd78e64e8c795a555894f4b8624f99b9e3f115d0
ArdeshirV <e-job@protonmail.com>
2025-09-03 12:59:00 +0200

    Add RequestResponse interface and Route structs

--- main.go
+++ main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println(Prompt("My HTTP Server"))
 }
 
+type MyHandler func(server RequestResponse)
 
+type MyServer interface {
+  GET(route string, handler MyHandler)
+  POST(route string, handler MyHandler)
+  PATCH(route string, handler MyHandler)
+  PUT(route string, handler MyHandler)
+  DELETE(route string, handler MyHandler)
+}
+
+type RequestResponse interface {
+  Request() *http.Request 
+  Response() http.ResponseWriter
+}
+
+type myserver struct {
+  routes []Route
+}
+
+type Route struct {
+  parts []string
+  handler MyHandler
+  method string 
+}
